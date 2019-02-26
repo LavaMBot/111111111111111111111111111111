a63883326064ce14ac57ebfe726fc87b0ca6e91c
Lava <47275047+LavaMBot@users.noreply.github.com>
2019-02-26 12:50:43 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 ﻿const Discord = require("discord.js");
 const client = new Discord.Client();
 const client2 = new Discord.Client();
-const myid = [''];
+const myid = ['463233613454901249'];
 const prefix = ['-'];
 
 client.on('ready', () => {
